7d94b4cf7c0258eb24f246ca00033719a1c16c2f
Arunan Ramanathan <contact.arunan@gmail.com>
2021-07-18 16:19:52 +0530

    implemented trailing zero without BigInteger in Java.

--- java/src/com/algorithmdb/math/Factorial.java
+++ java/src/com/algorithmdb/math/Factorial.java
@@ -26,6 +26,7 @@ import java.math.BigInteger;
  *                        1. Iterative way of finding out the factorial
  *                        2. Recursive way of finding out the factorial
  *                        3. Find out how many trailing zeros in the result of the factorial
+ *                        4. Find out trailing zeros without bigint by calculating the factor 5.
  * ----------------------------------------------------------------------------------
  *
  * @author Arunan R
@@ -58,6 +59,23 @@ public class Factorial {
         return numberOfZeros;
     }
 
+    /**
+     * Find the factors and count the number of 5 in the factors.
+     * If n=10, then we have 2 5's. Every 5th number we have a single 5.
+     * But if n=25 we have 5*5 and if n=125 we have 5*5*5, we have to
+     * count all the fives which will give us the number of trailing zeros.
+     *
+     * @param number input
+     * @return number of zeros
+     */
+    public int countTrailingZerosWithoutBigInt(int number) {
+        int countZeros = 0;
+        for (int i=5; i<=number; i=i*5) {
+            countZeros = countZeros + number/i;
+        }
+        return countZeros;
+    }
+
     public static void main(String[] args) {
         System.out.println(new Factorial().recursiveFactorial(BigInteger.valueOf(4)));
         System.out.println(new Factorial().recursiveFactorial(BigInteger.valueOf(5)));
@@ -66,5 +84,6 @@ public class Factorial {
         System.out.println(new Factorial().countTrailingZeros(5));
         System.out.println(new Factorial().countTrailingZeros(10));
         System.out.println(new Factorial().countTrailingZeros(100));
+        System.out.println(new Factorial().countTrailingZerosWithoutBigInt(100));
     }
 }
